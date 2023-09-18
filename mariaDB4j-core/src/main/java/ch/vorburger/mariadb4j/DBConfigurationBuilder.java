ba091e6498a75f04ffed2c8aaa01d07c74093acc
Knowles Atchison <katchisonjr@gmail.com>
2023-09-18 17:16:00 -0400

    Point release of winx64 package update.

--- mariaDB4j-core/src/main/java/ch/vorburger/mariadb4j/DBConfigurationBuilder.java
+++ mariaDB4j-core/src/main/java/ch/vorburger/mariadb4j/DBConfigurationBuilder.java
@@ -305,7 +305,13 @@ public class DBConfigurationBuilder {
                 throw new IllegalStateException("OS not directly supported, please use setDatabaseVersion() to set the name "
                         + "of the package that the binaries are in, for: " + SystemUtils.OS_VERSION);
             }
-            databaseVersion = "mariadb-10.6.12";
+            // initial 10.6.12 winx64 artifact publish was broken
+            // point release made to work on Windows correctly
+            if (WINX64.equals(getOS())) {
+                databaseVersion = "mariadb-10.6.12.1";
+            } else {
+                databaseVersion = "mariadb-10.6.12";
+            }
         }
         return databaseVersion;
     }
